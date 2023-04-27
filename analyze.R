ef91930613197cff57203346b4f4e791938deb81
perej1 <jaakko.pere@aalto.fi>
2023-04-27 14:51:51 +0300

    Add bivariate analysis

--- analyze.R
+++ analyze.R
@@ -3,6 +3,11 @@ library(dplyr)
 library(purrr)
 library(ellipticalsymmetry)
 library(robustbase)
+library(purrr)
+library(tidyr)
+library(reshape2)
+library(stringr)
+library(ggplot2)
 
 
 #' Generate m equally spaced points from a circle
@@ -32,6 +37,7 @@ sqrtmat <- function(sigma) {
   eigenvec <- eigen(sigma)$vectors
   eigenvec %*% diag(eigenval^0.5) %*% t(eigenvec)
 }
+
 
 #' Estimate elliptical extreme quantile region
 #'
@@ -87,6 +93,7 @@ compute_test_stat_regvar <- function(r, eta = 0.5, m = 10000, k = 80) {
   k * mean((d / gamma + log(t))^2 * t^eta)
 }
 
+
 #' Simulate Observation from Asymptotic Distribution of the PE Test Statistic
 #'
 #' @param eta Double. Free parameter, see (Hüsler and Li 2006) for details.
@@ -119,7 +126,9 @@ test_regvar <- function(r, eta = 0.5, m = 10000, k = 80, mm  = 10000) {
 }
 
 
+# Read data
 stock <- read_csv("data/stock-price-mod.csv", col_names = TRUE)
+parameters <- read_csv("data/model-parameters.csv", col_names = TRUE)
 
 # Ljung-box test for returns
 box_test_return <- stock %>%
@@ -132,12 +141,12 @@ box_test_return$jpn_return$p.value
 
 # Ljung-box test for innovations
 box_test_innovation <- stock %>%
-  select(contains("innovation")) %>%
+  select(ends_with("innovation")) %>%
   map(~ Box.test(., lag = 20, type = "Ljung-Box"))
 
-box_test_innovation$us_return$p.value
-box_test_innovation$uk_return$p.value
-box_test_innovation$jpn_return$p.value
+box_test_innovation$us_innovation$p.value
+box_test_innovation$uk_innovation$p.value
+box_test_innovation$jpn_innovation$p.value
 
 # Test for ellipticity (Huffer and Park 2007)
 ellipticity_test <- stock %>%
@@ -148,18 +157,80 @@ ellipticity_test <- stock %>%
 ellipticity_test$p.value
 
 # Visual inspection of the ellipticity assumption
-plot(stock$us_return_innovation, stock$uk_return_innovation)
-plot(stock$us_return_innovation, stock$jpn_return_innovation)
-plot(stock$uk_return_innovation, stock$jpn_return_innovation)
+plot(stock$us_innovation, stock$uk_innovation)
+plot(stock$us_innovation, stock$jpn_innovation)
+plot(stock$uk_innovation, stock$jpn_innovation)
 
 # Test regular variation of the Mahalanobis distance
 sigma <- stock %>%
-  select(contains("innovation")) %>%
+  select(ends_with("innovation")) %>%
   covMcd(alpha = 0.5)
 
 reg_test <- stock %>%
-  select(contains("innovation")) %>%
+  select(ends_with("innovation")) %>%
   as.matrix %>%
   stats::mahalanobis(center = sigma$center, cov = sigma$cov) %>%
-  #sqrt() %>%
+  sqrt() %>%
   test_regvar(eta = 0.5, m = 10000, k = 80, mm = 10000)
+
+# Compute extreme quantile estimates for innovations
+innovation_comb <- stock %>%
+  select(ends_with("innovation")) %>%
+  combn(2, simplify = FALSE)
+
+prediction_comb <- stock %>%
+  select(ends_with("prediction")) %>%
+  combn(2, simplify = FALSE)
+
+m <- 1000
+k <- 160
+p <- c(low = 1 / 2000, medium = 1 / 5000, high = 1 / 10000)
+labels <- c(us = "S&P 500", uk = "FTSE 100", jpn = "Nikkei 225")
+
+for (i in 1:length(innovation_comb)) {
+  countries <- c(str_split(names(innovation_comb[[i]]), "_",
+                           simplify = TRUE))[1:2]
+  sigma <- parameters %>%
+    filter(country %in% countries) %>%
+    pull(sigma_pred) %>%
+    diag()
+  
+  offset <- parameters %>%
+    filter(country %in% countries) %>%
+    pull(offset)
+  
+  mcd_est <- covMcd(innovation_comb[[i]], alpha = 0.5)
+  estimates <- map(p, ~ elliptical_extreme_qregion(innovation_comb[[i]],
+                                                           mcd_est$center,
+                                                           mcd_est$cov,
+                                                           ., k, m)) %>%
+    map(~ sweep(. %*% t(sigma), 2, offset, "+")) %>%
+    do.call(rbind, .) %>%
+    as_tibble() %>%
+    mutate(group = rep(c("low", "medium", "high"), each = m)) %>%
+    rename(x = V1, y = V2)
+  
+  colnames(prediction_comb[[i]]) <- c("x", "y")
+  g <- ggplot(prediction_comb[[i]], aes(x = x, y = y)) +
+    geom_point() +
+    geom_path(data = estimates,
+              aes(x = x, y = y, group = group, linetype = group),
+              show.legend = FALSE) +
+    coord_fixed() +
+    theme(panel.background = element_blank(),
+          axis.line = element_line(colour = "black"),
+          legend.background = element_blank(),
+          legend.key = element_blank(),
+          legend.key.size = unit(1, "cm"),
+          legend.text = element_text(size = 15),
+          axis.text = element_text(size = 15)) +
+    xlab(labels[countries[1]]) + ylab(labels[countries[2]]) +
+    scale_linetype_manual(name = NULL,
+                          values = c("low" = "solid", "medium" = "dashed",
+                                     "high" = "dotted"))
+  
+  # Save figure
+  filename <- str_c("figures/", str_c(countries, collapse = "_"), ".jpg")
+  ggsave(filename, plot = g, width = 7, height = 7,
+         dpi = 1000)
+}
